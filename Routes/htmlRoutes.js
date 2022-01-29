23736ac9adb8e65331ec60bb8322a1268e417219
Billy C <billyc.@Candices.local>
2022-01-28 21:23:12 -0800

    added Routes

---
+++ Routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const router = require("express").Router();
+
+//display homepage
+router.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "../Develop/Public/index.html"));
+});
+
+//display exercise page 
+router.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, "../Develop/Public/exercise.html"));
+});
+
+//display stats page
+router.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../Develop/Public/stats.html"));
+});
+
+module.exports = router
